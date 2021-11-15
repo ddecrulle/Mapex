fb8a8d0aa5d580975a3fa37cf34632a2f05dff37
Dylan Decrulle <dylan.decrulle@insee.fr>
2021-11-15 16:23:24 +0100

    update lunatic version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 echo "window._env_['LOCAL'] = '$LOCAL';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['PEARL_API_URL'] = '$PEARL_API_URL';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['GOOGLE_API_KEY'] = '$GOOGLE_API_KEY';" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['STROMAE_API_URL'] = '$STROMAE_API_URL';" >> /usr/share/nginx/html/env-config.js
 exec "$@"
